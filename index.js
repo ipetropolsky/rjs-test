e2bdfd63ca739f30da67373a2b3fa8d5b221b61a
ipetropolsky <i.petropolsky@hh.ru>
2017-04-10 20:53:01 +0300

    master Separate logs for builds

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const requirejs = require('requirejs');
 
+let logs = {};
+function log(name) {
+    logs[name] = logs[name] || [];
+    logs[name].push(Array.prototype.slice.call(arguments, 1));
+}
+function printLog(name) {
+    console.log("\nLog for " + name);
+    for (var i = 0; i < logs[name].length; i++) {
+        console.log.apply(console, logs[name][i]);
+    }
+}
+
 requirejs.optimize({
     baseUrl: 'src',
     generateSourceMaps: true,
     optimize: 'none',
     include: ['a', 'c', 'd'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/a-c-d.js', moduleName, filePath);
         return contents;
     },
     out: 'build/a-c-d.js'
-});
+}, () => printLog('build/a-c-d.js'));
 
 
 
@@ -21,11 +33,11 @@ requirejs.optimize({
     include: ['a', 'c', 'd'],
     exclude: ['b'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/a-c-d-exclude-b.js', moduleName, filePath);
         return contents;
     },
     out: 'build/a-c-d-exclude-b.js'
-});
+}, () => printLog('build/a-c-d-exclude-b.js'));
 
 requirejs.optimize({
     baseUrl: 'src',
@@ -34,11 +46,11 @@ requirejs.optimize({
     include: ['a', 'c', 'd'],
     excludeShallow: ['b'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/a-c-d-excludeShallow-b.js', moduleName, filePath);
         return contents;
     },
     out: 'build/a-c-d-excludeShallow-b.js'
-});
+}, () => printLog('build/a-c-d-excludeShallow-b.js'));
 
 requirejs.optimize({
     baseUrl: 'src',
@@ -47,11 +59,11 @@ requirejs.optimize({
     include: ['a', 'c', 'd'],
     stubModules: ['b'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/a-c-d-stubModules-b.js', moduleName, filePath);
         return contents;
     },
     out: 'build/a-c-d-stubModules-b.js'
-});
+}, () => printLog('build/a-c-d-stubModules-b.js'));
 
 
 
@@ -62,11 +74,11 @@ requirejs.optimize({
     include: ['a', 'b', 'c', 'd'],
     exclude: ['b'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/a-b-c-d-exclude-b.js', moduleName, filePath);
         return contents;
     },
     out: 'build/a-b-c-d-exclude-b.js'
-});
+}, () => printLog('build/a-b-c-d-exclude-b.js'));
 
 requirejs.optimize({
     baseUrl: 'src',
@@ -75,11 +87,11 @@ requirejs.optimize({
     include: ['a', 'b', 'c', 'd'],
     excludeShallow: ['b'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/a-b-c-d-excludeShallow-b.js', moduleName, filePath);
         return contents;
     },
     out: 'build/a-b-c-d-excludeShallow-b.js'
-});
+}, () => printLog('build/a-b-c-d-excludeShallow-b.js'));
 
 requirejs.optimize({
     baseUrl: 'src',
@@ -88,11 +100,11 @@ requirejs.optimize({
     include: ['a', 'b', 'c', 'd'],
     stubModules: ['b'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/a-b-c-d-stubModules-b.js', moduleName, filePath);
         return contents;
     },
     out: 'build/a-b-c-d-stubModules-b.js'
-});
+}, () => printLog('build/a-b-c-d-stubModules-b.js'));
 
 
 
@@ -102,11 +114,11 @@ requirejs.optimize({
     optimize: 'none',
     include: ['b', 'c', 'd'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/b-c-d.js', moduleName, filePath);
         return contents;
     },
     out: 'build/b-c-d.js'
-});
+}, () => printLog('build/b-c-d.js'));
 
 requirejs.optimize({
     baseUrl: 'src',
@@ -115,11 +127,11 @@ requirejs.optimize({
     include: ['b', 'c', 'd'],
     exclude: ['a'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/b-c-d-exclude-a.js', moduleName, filePath);
         return contents;
     },
     out: 'build/b-c-d-exclude-a.js'
-});
+}, () => printLog('build/b-c-d-exclude-a.js'));
 
 requirejs.optimize({
     baseUrl: 'src',
@@ -128,11 +140,11 @@ requirejs.optimize({
     include: ['b', 'c', 'd'],
     excludeShallow: ['a'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/b-c-d-excludeShallow-a.js', moduleName, filePath);
         return contents;
     },
     out: 'build/b-c-d-excludeShallow-a.js'
-});
+}, () => printLog('build/b-c-d-excludeShallow-a.js'));
 
 requirejs.optimize({
     baseUrl: 'src',
@@ -141,8 +153,8 @@ requirejs.optimize({
     include: ['b', 'c', 'd'],
     stubModules: ['a'],
     onBuildRead: (moduleName, filePath, contents) => {
-        console.log(moduleName, filePath);
+        log('build/b-c-d-stubModules-a.js', moduleName, filePath);
         return contents;
     },
     out: 'build/b-c-d-stubModules-a.js'
-});
+}, () => printLog('build/b-c-d-stubModules-a.js'));
